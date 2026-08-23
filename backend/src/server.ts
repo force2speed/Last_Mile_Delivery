eb01620260c326d22c8bf1865e169086a8794d21
dhruv dhoundiyal <dhruvdhoundiyal@gmail.com>
2026-08-23 22:03:15 +0530

    Make CORS dynamically permissive to fix Render environment propagation issues

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT ?? 4000;
 
 // -- Middleware ----------------------------------------------------------------
 app.use(cors({
-  origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
+  origin: (origin, callback) => callback(null, true),
   credentials: true,
 }));
 app.use(express.json());
@@ -50,4 +50,5 @@ app.listen(PORT, () => {
 
 
 export default app;
+
 
